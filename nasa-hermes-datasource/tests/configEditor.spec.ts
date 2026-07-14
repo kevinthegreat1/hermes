691de72b97d573751fe3f5fdc7bfbbaf1faea28d
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2026-07-14 15:49:31 -0700

    Fix invalid config test

--- nasa-hermes-datasource/tests/configEditor.spec.ts
+++ nasa-hermes-datasource/tests/configEditor.spec.ts
@@ -31,6 +31,7 @@ test('"Save & test" should fail when configuration is invalid', async ({
 }) => {
   const ds = await readProvisionedDataSource<MyDataSourceOptions, MySecureJsonData>({ fileName: 'datasources.yml' });
   const configPage = await createDataSourceConfigPage({ type: ds.type });
+  await page.getByRole('textbox', { name: 'Hermes' }).fill(ds.jsonData.hermes ?? '');
   await expect(configPage.saveAndTest()).not.toBeOK();
   await expect(configPage).toHaveAlert('error', { hasText: 'Host configuration parameter is missing' });
 });
